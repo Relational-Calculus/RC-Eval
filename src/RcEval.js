554a969afc88b39b84d9ec383418e9265c706e13
Hele3796 <helene.mollerjensen2@gmail.com>
2023-10-10 11:17:39 +0200

    included more buttons

--- src/RcEval.js
+++ src/RcEval.js
@@ -138,6 +138,10 @@ export default function RcEval() {
               <OpperatorButton query={formState.query} setFormState={setFormState} icon={"∧"} cursorPosition={formState.cursor}/>
               <OpperatorButton query={formState.query} setFormState={setFormState} icon={"∨"} cursorPosition={formState.cursor}/>
               <OpperatorButton query={formState.query} setFormState={setFormState} icon={"∃"} cursorPosition={formState.cursor}/>
+              <OpperatorButton query={formState.query} setFormState={setFormState} icon={"¬"} cursorPosition={formState.cursor}/>
+              <OpperatorButton query={formState.query} setFormState={setFormState} icon={"⇒"} cursorPosition={formState.cursor}/> 
+              <OpperatorButton query={formState.query} setFormState={setFormState} icon={"∀"} cursorPosition={formState.cursor}/>
+              <OpperatorButton query={formState.query} setFormState={setFormState} icon={"="} cursorPosition={formState.cursor}/>
             </ButtonGroup>
             <QueryTextField query={formState.query} setFormState={setFormState} />
           </Grid>
