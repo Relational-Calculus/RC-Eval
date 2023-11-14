d51aab62a51fced6bebf8cebf1d394092e3b1bce
Johannes Rosendal <rosendal.johannes@gmail.com>
2023-11-15 00:29:57 +0100

    switching branch

--- src/components/CodeEditor.js
+++ src/components/CodeEditor.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useCodeMirror } from '@uiw/react-codemirror';
 import { RC } from '../lang-rc/index.js';
 import { createTheme } from '@uiw/codemirror-themes';
